05ae628c3c79cb2fe929ca19100ca68a0072dac4
Viacheslav Danylko <gamerdvv@gmail.com>
2024-09-06 15:40:04 +0300

    fix code to file task3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,6 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-input.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-  const trimmedValue = event.currentTarget.value.trim();
-  output.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
-}
+input.addEventListener('input', ({ currentTarget }) => {
+  output.textContent = currentTarget.value.trim() || 'Anonymous';
+});
